27065375e647d622d86ec751582cdb86e5388b9f
Lenovo <mikrise2@mail.ru>
2021-04-12 13:01:18 +0300

    updated Securityhandler

--- src/main/java/com/example/demo/handlers/Securityhandler.java
+++ src/main/java/com/example/demo/handlers/Securityhandler.java
@@ -7,6 +7,7 @@ import java.util.Date;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.example.demo.domain.User;
 import com.example.demo.repos.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
@@ -19,7 +20,7 @@ public class Securityhandler implements AuthenticationSuccessHandler {
     private UserRepo userRepo;
 
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
-        var user = userRepo.findByEmail(authentication.getName());
+        User user = userRepo.findByEmail(authentication.getName());
         user.setDateOfLastLogin(new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(new Date()));
         userRepo.save(user);
         response.sendRedirect("/");
